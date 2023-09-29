242f1fefda4c371ce0aa526032879c02dc466f26
Daniel Stephanus Najoan <41951274+danielnajoan@users.noreply.github.com>
2023-09-30 01:24:00 +0700

    Update 1.1

--- src/test/java/com/microservices/java/movies/ApplicationTests.java
+++ src/test/java/com/microservices/java/movies/ApplicationTests.java
@@ -1,36 +1,13 @@
 package com.microservices.java.movies;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.context.WebApplicationContext;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@AutoConfigureMockMvc
 class ApplicationTests {
-	
-	@Autowired
-	private WebApplicationContext webApplicationContext;
-	
-	@Autowired
-	private MockMvc mockMvc;
     
-//	@Test
-//	void contextLoads() {
-//	}
-	
 	@Test
-	public void shouldReturnDefaultMessage() throws Exception {
-		this.mockMvc.perform(get("/movies/")).andDo(print()).andExpect(status().isOk())
-				.andExpect(content().contentType("application/json"))
-				.andReturn();
+	void contextLoads() {
 	}
 
 }
